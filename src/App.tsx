2a6f952ed15d5ecac07e58b0a9104a51c8c36911
Pamela Nachbar <nachbarpamela@gmail.com>
2025-06-09 14:41:40 -0300

    Fluxo de entrada do Vite

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './assets/style/global.css'
+import './assets/style/theme.css'
+import { Heading } from './components/Heading';
+export function App(){
+    console.log(`Ola mundo`);
+    return  <div>
+        <Heading></Heading>
+        <h1>Ola Mundo!</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+         Deserunt maxime placeat, ea et consequuntur neque blanditiis dolores natus 
+         cumque porro laudantium perspiciatis cum voluptatum aliquid accusamus enim
+         ab corporis ad.</p>
+
+    </div>
+    
+}
+
---
+++ src/components/Heading.tsx
@@ -0,0 +1,7 @@
+import styles from './Heading.module.css';
+
+export function Heading(){
+   // const classes = `${styles.heading} ${styles.purple}`
+    return <h1 className= 'heading purple'>Ola, mundo!</h1>
+    
+}
