b3deab20d07a1f0081e6b2f606aa52b9f57e0e4b
Pamela Nachbar <nachbarpamela@gmail.com>
2025-06-12 15:11:14 -0300

    Fluxo de entrada do Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 
 import "./assets/style/global.css";
 import "./assets/style/theme.css";
-import { Container } from "./components/Container/";
-import { CountDown } from "./components/CountDown";
-import { Logo } from "./components/Logo";
-import { Menu } from "./components/Menu";
-import { Footer } from "./components/Footer";
-import { MainForm } from "./components/MainForm";
+import { Home } from "./pages/Home";
 
 
 export function App() {
  
   return (
-    <>
-      <Container><Logo/></Container>
-      <Container><Menu/></Container>
-      <Container><CountDown/></Container>
-      <Container><MainForm></MainForm></Container>
-
-      <Container><Footer></Footer></Container>
-    </>
+   <Home/>
   );
 }
